903effcccd4ef0a7d8cccc283699ba02c0b165fc
Chris Beach <beachcoding47@gmail.com>
2017-06-22 16:38:23 -0500

    Imported 6.4-QA3 API docs since no changes were made in this area from 6.4-QA3 to 6.5-GA

---
+++ api-docs/Script/api/docscripts.js
@@ -0,0 +1,56 @@
+
+function hasNamePart(element,namePart)
+{
+    if (!element)
+    {
+        return false;
+    }
+
+    var upperNamePart = namePart.toUpperCase();
+
+    if (element.className)
+    {
+        var nameParts=element.className.split(' ');
+
+        for (var i=0; i < nameParts.length; i++)
+        {
+            if (nameParts[i].toUpperCase() == upperNamePart)
+            {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+function addNamePart(element,namePart)
+{
+    if (!hasNamePart(element,namePart))
+    {
+        element.className += element.className ? (" "+namePart) : namePart;
+    }
+}
+
+function removeNamePart(element,namePart)
+{
+    var upperNamePart = namePart.toUpperCase();
+
+    var remainingClasses=[];
+
+    if (element.className)
+    {
+        var nameParts = element.className.split(' ');
+
+        for (var i=0; i < nameParts.length; i++)
+        {
+            if (nameParts[i].toUpperCase() != upperNamePart)
+            {
+                remainingClasses[remainingClasses.length] = nameParts[i];
+            }
+        }
+
+        element.className = remainingClasses.join(' ');
+    }
+}
+
